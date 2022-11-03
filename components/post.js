8c9e8b88505f8701a3a072cab08eeea1ab230643
JabosCOL <cesar.bolivarc@hotmail.com>
2022-11-03 08:57:55 -0500

    Finalizando pags de inicio y blog

---
+++ components/post.js
@@ -0,0 +1,31 @@
+import Image from "next/future/image"
+import Link from "next/link"
+import { formatearFecha } from "../utils/helpers"
+import styles from "../styles/blog.module.css"
+
+export default function Post({post}) {
+
+    console.log(post)
+
+    const { contenido, imagen, titulo, url, publishedAt } = post
+
+  return (
+    <article>
+        <Image src={imagen.data.attributes.formats.medium.url} 
+            alt={`Imagen blog ${titulo}`} 
+            width={600}
+            height={400}
+        />
+        <div className={styles.contenido}>
+            <h3>{titulo}</h3>
+            <p className={styles.fecha}>{formatearFecha(publishedAt)}</p>
+            <p className={styles.resumen}>{contenido}</p>
+            <Link href={`/blog/${url}`}>
+                <a className={styles.enlace}>
+                    Leer post
+                </a>
+            </Link>
+        </div>
+    </article>
+  )
+}
